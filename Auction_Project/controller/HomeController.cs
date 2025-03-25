713d6e3b11ca8a0deb982497e8fd4f2c46864a01
Muhammadd-01 <Muhammadaffan1445@gmail.com>
2025-03-25 12:23:54 +0500

    changes

---
+++ Auction_Project/controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auction_Project.controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
