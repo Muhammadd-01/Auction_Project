d728221bdea3730b2a2c6bb01b3f14b9612db155
Muhammadd-01 <Muhammadaffan1445@gmail.com>
2025-03-29 19:10:59 +0500

    changes

--- Auction_Project/controller/AdminController.cs
+++ Auction_Project/controller/AdminController.cs
@@ -1,10 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Auction_Project.controller
+namespace YourProject.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult ManageUsers()
+        {
+            return View();
+        }
+
+        public IActionResult ManageBids()
+        {
+            return View();
+        }
+
+        public IActionResult Reports()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
         {
             return View();
         }
