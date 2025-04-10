ed6a0978dad9e65cf989c918e0bb55d990fcc6cf
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-04-10 15:38:47 +0500

    Database

--- Auction_Project/controller/AdminController.cs
+++ Auction_Project/controller/AdminController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Auction_Project.models;
+using System.Linq;
 
 namespace Auction_Project.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly AuctionClass _context;
+
+        public AdminController(AuctionClass context)
+        {
+            _context = context;
+        }
+
         public IActionResult Dashboard()
         {
             return View();
@@ -11,41 +20,61 @@ namespace Auction_Project.Controllers
 
         public IActionResult ManageUsers()
         {
-            return View();
+            var users = _context.tbl_Users.ToList(); // Fetch users from SQL Server
+            return View(users);
         }
+        [HttpPost]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = _context.tbl_Users.FirstOrDefault(u => u.id == id);
+            if (user != null)
+            {
+                _context.tbl_Users.Remove(user);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("ManageUsers"); // Redirect back to the user management page after deletion
+        }
+
         public IActionResult AddUsers()
         {
             return View();
         }
+
         public IActionResult ManageItems()
         {
             return View();
         }
+
         public IActionResult AddItems()
         {
             return View();
         }
+
         public IActionResult ManageCategories()
         {
             return View();
         }
+
         public IActionResult Books()
         {
             return View();
-
         }
+
         public IActionResult Furnitures()
         {
             return View();
         }
+
         public IActionResult Electronics()
         {
             return View();
         }
+
         public IActionResult Page404()
         {
             return View();
         }
+
         public IActionResult ManageAuctions()
         {
             return View();
@@ -65,7 +94,5 @@ namespace Auction_Project.Controllers
         {
             return View();
         }
-
-      
     }
 }
