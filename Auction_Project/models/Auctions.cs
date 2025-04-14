847711c497afa1bef5288d5eadf06081433f875c
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-04-14 10:09:25 +0500

    Tables

--- Auction_Project/models/Auctions.cs
+++ Auction_Project/models/Auctions.cs
@@ -1,7 +1,29 @@
-﻿namespace Auction_Project.models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Auction_Project.models
 {
     public class Auctions
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } // "Samsung Galaxy S21", "Old Painting", etc.
+
+        public string Description { get; set; } // Item details
+
+        [Required]
+        public decimal StartingPrice { get; set; } // Base bid
+
+        public decimal? CurrentHighestBid { get; set; } // Gets updated as people bid
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public bool IsActive { get; set; } = true; // Auction still going on?
+
+        public string ImageUrl { get; set; } // Optional image
 
     }
 }
