2c2b4942c9062b9e272885feba4c1afcb5e5e78f
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-04-23 18:06:14 +0500

    Auction Project

---
+++ Auction_Project/Migrations/20250423075846_table.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Auction_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tbl_Users_tbl_Users_Usersid",
+                table: "tbl_Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tbl_Users_Usersid",
+                table: "tbl_Users");
+
+            migrationBuilder.DropColumn(
+                name: "Usersid",
+                table: "tbl_Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Usersid",
+                table: "tbl_Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_Users_Usersid",
+                table: "tbl_Users",
+                column: "Usersid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tbl_Users_tbl_Users_Usersid",
+                table: "tbl_Users",
+                column: "Usersid",
+                principalTable: "tbl_Users",
+                principalColumn: "id");
+        }
+    }
+}
